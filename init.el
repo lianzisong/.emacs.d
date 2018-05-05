2333312a5f74c2ab7e2e12b349ff8e1bde6145ab
lianzss <lianzs@Jason-PC.localdomain>
2018-05-05 19:00:13 +0800

    add the xcscope

--- init.el
+++ init.el
@@ -4,25 +4,53 @@
        ("melpa stable" . "http://elpa.emacs-china.org/melpa-stable/")))
 
 (package-initialize)
+;;disable the welcome page
 (setq inhibit-splash-screen t)
 
-;;python
+;;encoding
+(set-language-environment "chinese-gbk")
+;;(set-language-environment "utf-8")
 
+;;use-package
+(require 'use-package)
+
+
+;;================dev config ========
+;;python
+(require 'elpy)
 ;;set the rpc python to python3
-(setq elpy-rpc-python-command "python3")
 ;;set the interactiv python to python3
-(setq python-shell-interpreter "python3")
-(elpy-enable)
+(use-package elpy
+  :config
+  (setq elpy-rpc-python-command "python3")
+  (setq python-shell-interpreter "python3")
+  (elpy-enable))
 
 ;;python end
 
-;;irony
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'objc-mode-hook 'irony-mode)
+;;company
+(require 'company)
+(add-hook 'c++-mode-hook '(lambda() (company-mode)))
+(add-hook 'c-mode-hook '(lambda() (company-mode)))
+(add-hook 'objc-mode-hook '(lambda() (company-mode)))
 
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-;;irony end
+;;company end
 
+;;cscope
+(require 'xcscope)
+(add-hook 'c++-mode-hook '(lambda() (cscope-enable)))
+(add-hook 'c-mode-hook '(lambda() (cscope-enable)))
+;;bind shortcut
+(use-package xcscope
+  :bind (("C-c s s" . cscope-find-this-symbol)
+	 ("C-c s d" . cscope-find-global-definition)
+	 ("C-c s n" . cscope-history-forward-line-current-result)
+	 ("C-c s p" . cscope-history-backward-line-current-result)
+	 ("C-c s c" . cscope-find-functions-calling-this-function)
+	 ("C-c s u" . cscope-pop-mark))
+  )
 
+;;cscope end
+
+;;==========================end=======================
 (put 'erase-buffer 'disabled nil)
